8104151fade5fd38ba67d6f76b92c31e661b7cb7
Jon Eyolfson <jon@eyl.io>
2018-07-05 16:58:05 -0400

    Added solarized to emacs

--- common/emacs/home/emacs.d/init.el
+++ common/emacs/home/emacs.d/init.el
@@ -1,9 +1,27 @@
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(column-number-mode 1)
-
-(setq make-backup-files nil)
+(add-to-list 'custom-theme-load-path "/usr/share/emacs/etc/themes/")
 
 (require 'evil)
-(evil-mode 1)
+(require 'uniquify)
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(evil-default-cursor t)
+ '(evil-mode t)
+ '(frame-background-mode (quote dark))
+ '(make-backup-files nil)
+ '(menu-bar-mode nil)
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
+
+(load-theme 'solarized t)
