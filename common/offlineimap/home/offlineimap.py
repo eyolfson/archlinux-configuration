a0376d68ef825811c0dd0f6b2b80e952d3b53c02
Jon Eyolfson <jon@eyl.io>
2019-01-15 08:34:50 -0400

    Updated offlineimap configuration

--- common/offlineimap/home/offlineimap.py
+++ common/offlineimap/home/offlineimap.py
@@ -2,13 +2,6 @@
 
 import subprocess
 
-NAMETRANS_EYL_PAIRS = [
-    ('Drafts', 'drafts'),
-    ('INBOX', 'inbox'),
-    ('Sent', 'sent'),
-    ('Trash', 'trash')
-]
-
 NAMETRANS_UWATERLOO_PAIRS = [
     ('INBOX.Drafts', 'drafts'),
     ('INBOX', 'inbox'),
@@ -19,18 +12,6 @@ NAMETRANS_UWATERLOO_PAIRS = [
 
 def get_password(name):
     return subprocess.check_output(['pass', 'show', name]).splitlines()[0]
-
-def nametrans_eyl_local(name):
-    for remote, local in NAMETRANS_EYL_PAIRS:
-        if name == local:
-            return remote
-    return name
-
-def nametrans_eyl_remote(name):
-    for remote, local in NAMETRANS_EYL_PAIRS:
-        if name == remote:
-            return local
-    return name
 
 def nametrans_uwaterloo_local(name):
     for remote, local in NAMETRANS_UWATERLOO_PAIRS:
