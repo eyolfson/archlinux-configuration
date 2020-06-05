306d40712cfb53e5490b4992217fb564a422c079
Jon Eyolfson <jon@eyl.io>
2020-06-05 14:37:27 -0700

    Added version output

--- archlinux_configuration/__init__.py
+++ archlinux_configuration/__init__.py
@@ -4,6 +4,11 @@ from .configs import check_configs
 from .packages import check_packages
 from .repos import check_repos
 
+from .constants import BASE_DIR
+
+import os
+import subprocess
+
 def get_hostname():
     path = '/etc/hostname'
     try:
@@ -16,8 +21,18 @@ def get_hostname():
         exit(1)
     return contents[:-1]
 
+def get_version():
+    p = subprocess.run([os.path.join(BASE_DIR, 'version.sh')],
+                       check=True,
+                       stdout=subprocess.PIPE,
+                       text=True)
+    return p.stdout.strip()
+
 def main(args):
     hostname = get_hostname()
+    title = '\033[1;34mArch Linux Configuration\033[0m \033[36m{}\033[0m'
+    version = get_version()
+    print(title.format(version))
     check_packages(hostname)
     check_configs(hostname)
     check_repos()
--- archlinux_configuration/constants.py
+++ archlinux_configuration/constants.py
@@ -1,5 +1,5 @@
 import os
 
-SCRIPTS_DIR = os.path.dirname(os.path.realpath(__file__))
-BASE_DIR = os.path.dirname(SCRIPTS_DIR)
+CONSTANTS_DIR = os.path.dirname(os.path.realpath(__file__))
+BASE_DIR = os.path.dirname(CONSTANTS_DIR)
 HOST_SPECIFIC_DIR = os.path.join(BASE_DIR, 'host-specific')
